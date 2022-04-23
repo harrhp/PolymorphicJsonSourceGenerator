64ffd1afbc2c34e052545978bce93ad0e623ef08
harrhp <47701658+harrhp@users.noreply.github.com>
2022-04-23 04:11:33 +0300

    build with npb tool (#3)

--- build/Components/ArtifactsComponent.cs
+++
@@ -1,9 +0,0 @@
-using Nuke.Common.IO;
-using static Nuke.Common.NukeBuild;
-
-namespace _build.Components;
-
-public interface ArtifactsComponent
-{
-    static AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
-}
